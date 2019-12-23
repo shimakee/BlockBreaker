c265684e8911fad303e438938fb396ffe513bbbb
shimakee <deleon.services@outlook.com>
2019-12-23 17:43:31 +0800

    imported scene and scene changer, added image sprite

---
+++ BlockBreaker/Assets/SceneChanger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ChangeScene(int index)
+    {
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+        Debug.Log(sceneCount);
+
+        if (index >= sceneCount)
+            index = sceneCount - 1;
+        
+        SceneManager.LoadScene(index);
+    }
+}
